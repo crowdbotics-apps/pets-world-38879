ad77b59868d6be724f2492576ab45e4e79afe8f8
Crowdbotics <team@crowdbotics.com>
2023-02-13 17:10:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pets-world-38879",
+  url: "https://pets-world-38879.botics.co",
+  api: "https://pets-world-38879.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
